6f934044e45245ba023cf32a5c38ef196bbf66a5
oblivioussing <oblivioussing@outlook.com>
2021-09-30 18:06:17 +0800

    cell

--- lib/package/cell/cell.dart
+++ lib/package/cell/cell.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ChantCell extends StatelessWidget {
-  const ChantCell({Key? key}) : super(key: key);
+  const ChantCell({
+    Key? key,
+    this.title = '',
+    this.value = '',
+    this.label = '',
+    this.size = CellSize.normal,
+    this.icon,
+  }) : super(key: key);
+
+  final String title; // 左侧标题
+  final String value; // 右侧内容
+  final String label; // 标题下方的描述信息
+  final CellSize size; // 单元格大小
+  final String? icon; // 左侧图标
 
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+}
+
+// 尺寸
+enum CellSize {
+  large,
+  normal,
 }
